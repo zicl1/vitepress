59b48631c427f1405aac2c264821dea3bcbcd12e
胡金雅 <zichen1016@qq.com>
2024-08-07 23:58:05 +0800

    添加 logo 图片和修改首页配置

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,16 +2,25 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  lang: 'zh-CN',
+  lang: 'en-US',
   title: "Zichen 文档",
   description: "A VitePress Site",
   base: '/vitepress/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
+    logo: '/favicon.ico',
+
     nav: [
       { text: '首页', link: '/' },
       { text: '软件', link: '/markdown-examples' },
-      { text: '编程', link: '/api-examples' }
+      { text: '编程', link: '/api-examples' },
+      { 
+        text: '相关链接', 
+        items: [
+          { text: 'vitepress', link: 'https://vitepress.dev/' },
+          { text: '关于我', link: 'https://zicl1.github.io/about' }
+        ]
+      }
     ],
 
     sidebar: [
@@ -25,9 +34,14 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/zicl1/vitepress' },
+      { icon: 'discord', link: 'https://github.com/zicl1/vitepress' },
       { icon: 'twitter', link: 'https://github.com/zicl1/vitepress' },
-      { icon: 'instagram', link: 'https://github.com/zicl1/vitepress' }
-    ]
+      { icon: 'github', link: 'https://github.com/zicl1/vitepress' }
+    ],
+
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2024-present Zichen'
+    }
   }
 })
--- docs/index.md
+++ docs/index.md
@@ -3,23 +3,26 @@
 layout: home
 
 hero:
-  name: "My Awesome Project"
-  text: "A VitePress Site"
-  tagline: My great project tagline
+  name: "Zichen 文档"
+  text: "软件安装和编程技术"
+  tagline: 记不住的都在文档里！
+  image:
+    src: /hero.png
+    alt: Zichen 文档
   actions:
     - theme: brand
-      text: Markdown Examples
+      text: 软件
       link: /markdown-examples
     - theme: alt
-      text: API Examples
+      text: 编程
       link: /api-examples
 
 features:
-  - title: Feature A
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature B
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature C
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: 软件安装
+    details: php、apache、mysql、navicat、typora、oracle 等软件的安装教程。
+  - title: 编程技术
+    details: c# 常用代码。
+  - title: 电脑操作
+    details: 重装系统、虚拟机安装 Linux 系统、远程桌面、局域网 ftp 服务器等教程。
 ---
 
